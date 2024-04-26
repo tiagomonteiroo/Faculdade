1b696766326eca726b8a58595090b73659c670ca
tiagomonteiroo <167870432+tiagomonteiroo@users.noreply.github.com>
2024-04-26 16:53:52 +0100

    HomePage Changes

--- Projeto_GLUGLUFC/urls.py
+++ Projeto_GLUGLUFC/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('homepage/', include('homepage.urls')),
+    path('', include('homepage.urls')),
     path('admin/', admin.site.urls),
 ]
